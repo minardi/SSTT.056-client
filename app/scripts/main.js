b6305deacdce517d3b93404b740239e1a3a1baf6
Ilya <ilyaillych@gmail.com>
2014-08-01 15:42:26 +0300

    First

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Sstt056, $*/
+
+
+window.Sstt056 = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Sstt056.init();
+});
